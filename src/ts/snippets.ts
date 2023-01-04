03e855408c03147712f582a0d52e1a27251fde92
nilsbenz <nils.benz@icloud.com>
2023-01-05 00:28:41 +0100

    feat: add navigation

--- src/ts/snippets.ts
+++ src/ts/snippets.ts
@@ -11,10 +11,47 @@ if (ageText) {
   ageText.innerHTML = String(getAge());
 }
 
-document.querySelector('#contact-link')?.addEventListener('click', () => {
-  document.querySelector('#contact')?.scrollIntoView({ behavior: 'smooth' });
-});
+const scrollIntoView = (selector: string) =>
+  document.querySelector(selector)?.scrollIntoView({ behavior: 'smooth' });
+
+const handleEnterPressed = (selector: string) => (e: any) => {
+  if (e.key === 'Enter') {
+    scrollIntoView(selector);
+  }
+};
+
+document
+  .querySelector('#contact-link')
+  ?.addEventListener('click', () => scrollIntoView('#contact'));
+
+document
+  .querySelector('#contact-link')
+  ?.addEventListener('keypress', handleEnterPressed('#contact'));
 
 document.querySelector('#scroll-down-button')?.addEventListener('click', () => {
   document.querySelector('#about')?.scrollIntoView({ behavior: 'smooth' });
 });
+
+document
+  .querySelector('#nav-videos')
+  ?.addEventListener('click', () => scrollIntoView('#videos'));
+
+document
+  .querySelector('#nav-videos')
+  ?.addEventListener('keypress', handleEnterPressed('#videos'));
+
+document
+  .querySelector('#nav-software')
+  ?.addEventListener('click', () => scrollIntoView('#software'));
+
+document
+  .querySelector('#nav-software')
+  ?.addEventListener('keypress', handleEnterPressed('#software'));
+
+document
+  .querySelector('#nav-contact')
+  ?.addEventListener('click', () => scrollIntoView('#contact'));
+
+document
+  .querySelector('#nav-contact')
+  ?.addEventListener('keypress', handleEnterPressed('#contact'));
